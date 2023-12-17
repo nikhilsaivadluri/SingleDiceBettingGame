e561b3691e08927fa49a278eba2dbab162da967c
Nikhil <nikhilsai.v@gmail.com>
2023-12-17 19:27:35 +0530

    gh-pages

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import BetGame from "./routes/BetGame";
-
-export default function App() {
-  return (
-    <div className="App">
-      <BetGame />
-    </div>
-  );
-}
--- src/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const INCREASE_BET = "INCREASE_BET";
-export const UPDATE_BALANCE = "UPDATE_BALANCE";
-export const CLEAR_BETS = "CLEAR_BETS";
-export const ADD_BALANCE = "ADD_BALANCE";
-export const ALLOW_BETTING = "ALLOW_BETTING";
-export const STOP_BETTING = "STOP_BETTING";
-export const UPDATE_ROLLED_DICE = "UPDATE_ROLLED_DICE";
